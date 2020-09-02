f9160438e48fc20859bc3af3f56056fe0d6023b5
Guilherme de Souza Bueno <guilherme.sobu@outlook.com>
2020-09-02 19:48:41 -0300

    Comitando para fazer mais testes

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
     <body>
 
         <div id="app" class="container">
+            
 
-        
         </div>
 
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
