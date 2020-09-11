fbeb9098492e28918e89c993dbdd4cd838ae84ab
Guilherme de Souza Bueno <guilherme.sobu@outlook.com>
2020-09-11 18:27:44 -0300

    Redirecionamento para a página certa quando clica em pagina inicial

--- defaultTop.php
+++ defaultTop.php
@@ -19,14 +19,14 @@
 
 <!-- NAVBAR -->
     <nav class="rounded-0 navbar navbar-expand-lg navbar-light bg-dark">
-        <a class="border rounded px-1 py-0 navbar-brand text-black bg-white" href="/asamericana/"><strong>SASDH</strong></a>
+        <a class="border rounded px-1 py-0 navbar-brand text-black bg-white" href="/"><strong>SASDH</strong></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item active">
-                    <a class="nav-link text-white" href="/asamericana/">Página principal <span class="sr-only">(current)</span></a>
+                    <a class="nav-link text-white" href="/">Página principal <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle text-white" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
