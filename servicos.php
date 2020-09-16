857b2fd685dc31aebcc83a537da802030398ac19
Guilherme de Souza Bueno <guilherme.sobu@outlook.com>
2020-09-16 19:41:57 -0300

    Inserido mais coisas dentro do dropdown do CRAS

--- servicos.php
+++ servicos.php
@@ -5,14 +5,73 @@ include 'defaultTop.php';
 
 <div class="row-fluid pt-4 border-bottom pb-4">
 
-    <h3>CRAS e CREAS</h3>
+    <h3>CREAS</h3>
     <p>BREVE descrição sobre os serviços</p>
-    <a class="rounded-bottom-0 btn btn-dark w-100" data-toggle="collapse" href="#collapseCc" role="button" aria-expanded="false" aria-controls="collapseCc">
-        Mais informações sobre CRAS e CREAS
+    <a class="rounded-bottom-0 btn btn-dark w-100" data-toggle="collapse" href="#collapseCe" role="button" aria-expanded="false" aria-controls="collapseCe">
+        Mais informações sobre o CREAS
     </a>
-    <div class="collapse px-1" id="collapseCc">
+    <div class="collapse px-1" id="collapseCe">
         <div class="card card-body">
             <p>Informações aqui</p>
+        </div>
+    </div>
+
+</div>
+
+<div class="row-fluid pt-4 border-bottom pb-4">
+
+    <h3>CRAS</h3>
+    <p class="my-0">BREVE descrição sobre os serviços</p>
+    <a class="rounded-bottom-0 btn btn-dark w-100" data-toggle="collapse" href="#collapseCa" role="button" aria-expanded="false" aria-controls="collapseCa">
+        Mais informações sobre os CRAS
+    </a>
+    <div class="collapse px-1" id="collapseCa">
+        <div class="card card-body">
+            <p>
+                <address class="h6">
+
+                    <strong>CRAS Mathiensen</strong><br>
+                    Rua dos Tucanos, 270 - Vila Mathiensen<br>
+                    Telefone: (19) 3407-2770<br>
+                    E-mail: crasvm@americana.sp.gov.br<br>
+                    <br>
+
+                    <strong>CRAS Praia Azul</strong><br>
+                    Rua Maranhão, 1595 - Praia Azul<br>
+                    Telefone: (19) 3467-2256<br>
+                    E-mail: craspa@americana.sp.gov.br<br>
+                    <br>
+
+                    <strong>CRAS Jardim Nossa Senhora Aparecida</strong><br>
+                    Rua Caetano de Campos, s/n - Jd. N. Sra. Aparecida<br>
+                    Telefone: (19) 3469-2339<br>
+                    E-mail: crasnsa@americana.sp.gov.br<br>
+                    <br>
+
+                    <strong>CRAS São Jerônimo</strong><br>
+                    Rua Carlos Vassalo, 370 - São Jerônimo<br>
+                    Telefone: (19) 3462-7151<br>
+                    E-mail: crassj@americana.sp.gov.br<br>
+                    <br>
+
+                    <strong>CRAS Guanabara</strong><br>
+                    Rua da Tijuca, 182 - Jardim Guanabara<br>
+                    Telefone: (19) 3407-8002<br>
+                    E-mail: crasguanabara@americana.sp.gov.br<br>
+                    <br>
+
+                    <strong>CRAS São Manoel</strong><br>
+                    Rua São Thiago, 320 - São Manoel<br>
+                    Telefone: (19) 3468-6103<br>
+                    E-mail: crassmanoel@americana.sp.gov.br<br>
+
+                </address>
+            </p>
+
+            <a class="rounded-bottom-0 btn btn-dark w-100" data-toggle="collapse" href="#collapseCa" role="button" aria-expanded="false" aria-controls="collapseCa">
+                Reduzir tamanho
+            </a>
+
         </div>
     </div>
 
