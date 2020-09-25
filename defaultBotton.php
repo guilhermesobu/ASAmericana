091d880dbe6534f19f8d750e8b9fb3cdffc30e4b
Guilherme de Souza Bueno <guilherme.sobu@outlook.com>
2020-09-25 17:30:21 -0300

    DESK. Removi as integridades dos bootstraps da vida não sei se deveria fazer isso mas tava dando erros

--- defaultBotton.php
+++ defaultBotton.php
@@ -52,9 +52,9 @@
             </div>
             
             <script src="/scriptRC.js"></script>
-            <script src="/bootstrap/js/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-            <script src="/bootstrap/js/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-            <script src="/bootstrap/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+            <script src="/bootstrap/js/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+            <script src="/bootstrap/js/popper.min.js" crossorigin="anonymous"></script>
+            <script src="/bootstrap/js/bootstrap.min.js" crossorigin="anonymous"></script>
         </body>
 
     </html>
