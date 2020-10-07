52b9251f86c9165e1ba5b0123270b71dd660c008
Guilherme de Souza Bueno <guilherme.sobu@outlook.com>
2020-10-07 20:33:31 -0300

    Desk. adicionado o CopyRight

--- defaultBotton.php
+++ defaultBotton.php
@@ -42,13 +42,10 @@
 
                 </div>
 
-
-                <!-- COPYRIGHT -->
-                <div class="row pt-4 m-0 d-flex justify-content-around align-items-center">
-                    <p style="font-size: 10px;" class="border p-2 rounded-pill text-center">© 2020 - Desenvolvido por 
-                        <strong><a href="">Guilherme de Souza Bueno</a></strong>
-                    </p>
-                </div>
+                <?php 
+                    require 'copyright.php';
+                ?>
+                
             </div>
             
             <script src="/scriptRC.js"></script>
