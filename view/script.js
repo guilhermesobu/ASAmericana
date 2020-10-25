87e757bcd53ce679bf69ef3da768fc99d1a53291
Guilherme de Souza Bueno <guilherme.sobu@outlook.com>
2020-10-25 12:22:57 -0300

    Resolvido o problema de clique que não abria dropdowns do chrome em dispositivos moveis

--- view/script.js
+++ view/script.js
@@ -1,29 +1,33 @@
 
 // AQUI ESTÀ O EFEITO CONSTANTE DO NAVBAR
 // PERGUNTAS FREQUENTES
-var dropfrequentlyQuestion = document.querySelector('#dropfrequentlyQuestion');
-dropfrequentlyQuestion.addEventListener("mouseleave", () => {
-    dropfrequentlyQuestion.click();
-
-});
-var frequentlyQuestion = document.querySelector('#frequentlyQuestion');
-frequentlyQuestion.addEventListener("mouseenter", () => {
-    if (dropfrequentlyQuestion.classList[3] == undefined) {
-        frequentlyQuestion.click();
-    }
-});
+if (window.innerWidth > 767) {
+    var dropfrequentlyQuestion = document.querySelector('#dropfrequentlyQuestion');
+    dropfrequentlyQuestion.addEventListener("mouseleave", () => {
+        dropfrequentlyQuestion.click();
+    });
+    var frequentlyQuestion = document.querySelector('#frequentlyQuestion');
+    frequentlyQuestion.addEventListener("mouseenter", () => {
+        if (dropfrequentlyQuestion.classList[3] == undefined) {
+            frequentlyQuestion.click();
+        }
+    });
+}
 
 //SERVIÇOS
-var dropServices = document.querySelector('#dropServices');
-dropServices.addEventListener("mouseleave", () => {
-    dropServices.click();
-});
-var services = document.querySelector('#services');
-services.addEventListener("mouseenter", () => {
-    if (dropServices.classList[3] == undefined) {
-        services.click();
-    };
-});
+if (window.innerWidth > 767) {
+    var dropServices = document.querySelector('#dropServices');
+    console.log('1');
+    dropServices.addEventListener("mouseleave", () => {
+        dropServices.click();
+    });
+    var services = document.querySelector('#services');
+    services.addEventListener("mouseenter", () => {
+        if (dropServices.classList[3] == undefined) {
+            services.click();
+        };
+    });
+}
 
 
 // AQUI ESTÁ A ABERTURA DE NOVA ABA AO CLICAR NO PMAS
